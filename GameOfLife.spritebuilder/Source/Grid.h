21fe19f0af6ec661b424b92a7bc849e882c91885
Joshua Hung <chachaman122@Joshuas-MacBook-Pro-2.local>
2014-06-26 15:10:01 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,14 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Joshua Hung on 6/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+@end
---
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -0,0 +1,22 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Joshua Hung on 6/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+#import "Creature.h"
+
+// these are variables that cannot be changed
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
+
+@implementation Grid {
+    NSMutableArray *_gridArray;
+    float _cellWidth;
+    float _cellHeight;
+}
+
+@end
